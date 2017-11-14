49711dbb8455dab094fda50414c3a4d87e5c2cd4
navalprabhakar <navalprabhakar@gmail.com>
2017-11-14 16:30:37 +0530

    "NVLAPP-2: Added production dev environment configuration"

--- src/app/resume-lite/resume-lite.service.ts
+++ src/app/resume-lite/resume-lite.service.ts
@@ -6,22 +6,23 @@ import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class ResumeLiteService {
-  private _restUrl = 'https://nvlapi.herokuapp.com/cv';
+  private _restUrl = environment.apiHost + '/cv';
 
   constructor(private _http: HttpClient) {}
 
   getResume(): Observable<ApiResponse> {
     return this._http.get<ApiResponse>(this._restUrl)
       .map((res) => res)
-      .do(data => console.log('resume summary downloaded'))
+      .do(data => console.log('NVLAPI services responded with resume data'))
         .catch(this.handleError);
   }
 
   handleError(err: HttpErrorResponse) {
-    console.log(err.message);
+    console.log("NVLAPI services failed to respond, kindly try later - " + err.message);
     return Observable.throw(err.message);
   }
 }
