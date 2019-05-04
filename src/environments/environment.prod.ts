23f9e9186de8c51eb019e882837583f8da9e4bd6
Naval Prabhakar <naval.prabhakar@nagarro.com>
2019-05-04 23:37:31 +0530

    Enabling self signed ssl for AWS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiHost: "http://ec2-13-233-117-150.ap-south-1.compute.amazonaws.com:8080"
+  apiHost: "https://ec2-13-233-117-150.ap-south-1.compute.amazonaws.com:8443"
 };
